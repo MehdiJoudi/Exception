987d2850498cb84e45799cc2116989c86743c057
MehdiJoudi <145011857+MehdiJoudi@users.noreply.github.com>
2023-11-24 11:32:27 +0100

    Update SuperHotel.java

--- Exception/Esprit_java/Prosit 6/src/exception/SuperHotel.java
+++ Exception/Esprit_java/Prosit 6/src/exception/SuperHotel.java
@@ -1,29 +1,18 @@
 package exception;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author USER
- */
-
-
 public class SuperHotel {
     static int x[];
 
-
-    public void reserver(){
-    
-              x[0] = 1;     
+    public void reserver() {
+        try {
+            x[0] = 1;
+        } catch (NullPointerException e) {
+            System.out.println("Caught a NullPointerException: Array 'x' is not initialized.");
+        }
     }
-    
+
     public static void main(String args[]) {
-        
         SuperHotel s = new SuperHotel();
         s.reserver();
     }
 }
-
